53d077a8f8b1e841cca0c4ab8c1f8808f36400ab
FranBurgos <fpizarro1990@gmail.com>
2018-11-10 17:13:13 +0100

    #11

--- PlantaPiloto/PlantaPiloto/Form1.Designer.cs
+++ PlantaPiloto/PlantaPiloto/Form1.Designer.cs
@@ -40,12 +40,25 @@
             this.label3 = new System.Windows.Forms.Label();
             this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
             this.txtReceive = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.configuraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conexionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crearConfiguraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarConfiguraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarConfiguraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manualDeUsuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 49);
+            this.label1.Location = new System.Drawing.Point(743, 51);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 20);
             this.label1.TabIndex = 0;
@@ -54,14 +67,14 @@
             // cboPort
             // 
             this.cboPort.FormattingEnabled = true;
-            this.cboPort.Location = new System.Drawing.Point(115, 46);
+            this.cboPort.Location = new System.Drawing.Point(815, 48);
             this.cboPort.Name = "cboPort";
             this.cboPort.Size = new System.Drawing.Size(121, 28);
             this.cboPort.TabIndex = 1;
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(268, 45);
+            this.btnOpen.Location = new System.Drawing.Point(1005, 51);
             this.btnOpen.Name = "btnOpen";
             this.btnOpen.Size = new System.Drawing.Size(75, 35);
             this.btnOpen.TabIndex = 2;
@@ -71,7 +84,7 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(369, 46);
+            this.btnClose.Location = new System.Drawing.Point(1106, 52);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 34);
             this.btnClose.TabIndex = 3;
@@ -81,15 +94,15 @@
             // 
             // txtMessage
             // 
-            this.txtMessage.Location = new System.Drawing.Point(115, 91);
+            this.txtMessage.Location = new System.Drawing.Point(812, 92);
             this.txtMessage.Multiline = true;
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(639, 112);
+            this.txtMessage.Size = new System.Drawing.Size(369, 112);
             this.txtMessage.TabIndex = 4;
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(679, 209);
+            this.btnSend.Location = new System.Drawing.Point(1106, 210);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(75, 34);
             this.btnSend.TabIndex = 5;
@@ -99,7 +112,7 @@
             // 
             // btnReceive
             // 
-            this.btnReceive.Location = new System.Drawing.Point(663, 485);
+            this.btnReceive.Location = new System.Drawing.Point(1090, 486);
             this.btnReceive.Name = "btnReceive";
             this.btnReceive.Size = new System.Drawing.Size(91, 35);
             this.btnReceive.TabIndex = 7;
@@ -110,7 +123,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(43, 91);
+            this.label2.Location = new System.Drawing.Point(742, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 20);
             this.label2.TabIndex = 8;
@@ -119,7 +132,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(36, 255);
+            this.label3.Location = new System.Drawing.Point(723, 286);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(70, 20);
             this.label3.TabIndex = 9;
@@ -131,17 +144,107 @@
             // 
             // txtReceive
             // 
-            this.txtReceive.Location = new System.Drawing.Point(115, 282);
+            this.txtReceive.Location = new System.Drawing.Point(812, 283);
             this.txtReceive.Name = "txtReceive";
-            this.txtReceive.Size = new System.Drawing.Size(639, 197);
+            this.txtReceive.Size = new System.Drawing.Size(369, 197);
             this.txtReceive.TabIndex = 10;
             this.txtReceive.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configuraciónToolStripMenuItem,
+            this.conexionesToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1205, 33);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // configuraciónToolStripMenuItem
+            // 
+            this.configuraciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crearConfiguraciónToolStripMenuItem,
+            this.cargarConfiguraciónToolStripMenuItem,
+            this.modificarConfiguraciónToolStripMenuItem});
+            this.configuraciónToolStripMenuItem.Name = "configuraciónToolStripMenuItem";
+            this.configuraciónToolStripMenuItem.Size = new System.Drawing.Size(135, 29);
+            this.configuraciónToolStripMenuItem.Text = "Configuración";
+            // 
+            // conexionesToolStripMenuItem
+            // 
+            this.conexionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serieToolStripMenuItem,
+            this.otrosToolStripMenuItem});
+            this.conexionesToolStripMenuItem.Name = "conexionesToolStripMenuItem";
+            this.conexionesToolStripMenuItem.Size = new System.Drawing.Size(153, 29);
+            this.conexionesToolStripMenuItem.Text = "Comunicaciones";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manualDeUsuarioToolStripMenuItem,
+            this.ayudaToolStripMenuItem1,
+            this.acercaDeToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(75, 29);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // crearConfiguraciónToolStripMenuItem
+            // 
+            this.crearConfiguraciónToolStripMenuItem.Name = "crearConfiguraciónToolStripMenuItem";
+            this.crearConfiguraciónToolStripMenuItem.Size = new System.Drawing.Size(284, 30);
+            this.crearConfiguraciónToolStripMenuItem.Text = "Crear configuración";
+            // 
+            // cargarConfiguraciónToolStripMenuItem
+            // 
+            this.cargarConfiguraciónToolStripMenuItem.Name = "cargarConfiguraciónToolStripMenuItem";
+            this.cargarConfiguraciónToolStripMenuItem.Size = new System.Drawing.Size(284, 30);
+            this.cargarConfiguraciónToolStripMenuItem.Text = "Cargar configuración";
+            // 
+            // modificarConfiguraciónToolStripMenuItem
+            // 
+            this.modificarConfiguraciónToolStripMenuItem.Name = "modificarConfiguraciónToolStripMenuItem";
+            this.modificarConfiguraciónToolStripMenuItem.Size = new System.Drawing.Size(284, 30);
+            this.modificarConfiguraciónToolStripMenuItem.Text = "Modificar configuración";
+            // 
+            // serieToolStripMenuItem
+            // 
+            this.serieToolStripMenuItem.Name = "serieToolStripMenuItem";
+            this.serieToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.serieToolStripMenuItem.Text = "Serie";
+            // 
+            // otrosToolStripMenuItem
+            // 
+            this.otrosToolStripMenuItem.Name = "otrosToolStripMenuItem";
+            this.otrosToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.otrosToolStripMenuItem.Text = "Otros";
+            // 
+            // manualDeUsuarioToolStripMenuItem
+            // 
+            this.manualDeUsuarioToolStripMenuItem.Name = "manualDeUsuarioToolStripMenuItem";
+            this.manualDeUsuarioToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.manualDeUsuarioToolStripMenuItem.Text = "Manual de usuario";
+            // 
+            // ayudaToolStripMenuItem1
+            // 
+            this.ayudaToolStripMenuItem1.Name = "ayudaToolStripMenuItem1";
+            this.ayudaToolStripMenuItem1.Size = new System.Drawing.Size(252, 30);
+            this.ayudaToolStripMenuItem1.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 565);
+            this.ClientSize = new System.Drawing.Size(1205, 565);
             this.Controls.Add(this.txtReceive);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -152,10 +255,13 @@
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.cboPort);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
             this.Text = "Interfaz Planta Piloto";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,6 +280,18 @@
         private System.Windows.Forms.Label label3;
         private System.IO.Ports.SerialPort serialPort2;
         private System.Windows.Forms.RichTextBox txtReceive;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem configuraciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem crearConfiguraciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarConfiguraciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarConfiguraciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem conexionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem serieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manualDeUsuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
     }
 }
 
