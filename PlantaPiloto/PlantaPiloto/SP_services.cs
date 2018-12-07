88b4120b56c15f2d20ae2506dcda9a21681dee44
Francisco Crespo Diez <fpizarro1990@gmail.com>
2018-12-07 18:06:44 +0100

    #33

--- PlantaPiloto/PlantaPiloto/SP_services.cs
+++ PlantaPiloto/PlantaPiloto/SP_services.cs
@@ -118,13 +118,22 @@ namespace PlantaPiloto
                     string[] spLine = _serialPort.ReadLine().Split(';');
                     // Asignación del valor a la variable
 
-
-                    //CAMBIAR ASIGNACIÓN DE VALORES DEPENDIENDO SI SON DE LECTURA O ESCRITURA
-                    //VARIABLES DE ESCRITURA NO DEVUELVEN VALOR, HAY QUE ASIGNARLES EL VALOR DE VAR_ECO
                     if (_proyect.Variables.Count(p => p.Name == spLine[1]) > 0)
                     {
                         _proyect.Variables.FirstOrDefault(p => p.Name  == spLine[1]).Time = Int32.Parse(spLine[0]);
                         _proyect.Variables.FirstOrDefault(p => p.Name  == spLine[1]).Value = spLine[2];
+
+                        //Asigno valor a las variables que son sólo de escritura para tener una referencia en la vista
+                        if (spLine[1].EndsWith("_eco") && _proyect.Variables.Where(p => p.Name + "_eco" == spLine[1]).Count() != 0)
+                        {
+                            _proyect.Variables.FirstOrDefault(p => p.Name + "_eco" == spLine[1]).Time = Int32.Parse(spLine[0]);
+                            _proyect.Variables.FirstOrDefault(p => p.Name + "_eco" == spLine[1]).Value = spLine[2];
+                        }
+                        if (spLine[1].EndsWith("_x3_eco") && _proyect.Variables.Where(p => p.Name + "_x3_eco" == spLine[1]).Count() != 0)
+                        {
+                            _proyect.Variables.FirstOrDefault(p => p.Name + "_x3_eco" == spLine[1]).Time = Int32.Parse(spLine[0]);
+                            _proyect.Variables.FirstOrDefault(p => p.Name + "_x3_eco" == spLine[1]).Value = spLine[2];
+                        }
                     }
                     // Comprobación que todas las variables tienen valor y llamada al método que las guarda en la BD
                     // Se crea el requisito de que todas las variables del proyecto deben existir en la placa
