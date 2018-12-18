b07f97b56f342de60f1aa75c9069a962d31ea8e6
Francisco Crespo Diez <fpizarro1990@gmail.com>
2018-12-18 20:23:50 +0100

    #20

--- PlantaPiloto/PlantaPiloto/AboutForm.Designer.cs
+++ PlantaPiloto/PlantaPiloto/AboutForm.Designer.cs
@@ -32,13 +32,13 @@
             this.btnAboutAccept = new System.Windows.Forms.Button();
             this.gbAbout = new System.Windows.Forms.GroupBox();
             this.lblAboutDesc = new System.Windows.Forms.Label();
-            this.lblAboutDegree = new System.Windows.Forms.Label();
-            this.lblAboutUni = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblAboutDegree = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblAboutUni = new System.Windows.Forms.Label();
             this.gbAbout.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAboutAccept
@@ -59,9 +59,9 @@
             this.gbAbout.Controls.Add(this.pictureBox1);
             this.gbAbout.Controls.Add(this.lblAboutUni);
             this.gbAbout.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gbAbout.Location = new System.Drawing.Point(24, 14);
+            this.gbAbout.Location = new System.Drawing.Point(24, 12);
             this.gbAbout.Name = "gbAbout";
-            this.gbAbout.Size = new System.Drawing.Size(311, 206);
+            this.gbAbout.Size = new System.Drawing.Size(311, 208);
             this.gbAbout.TabIndex = 26;
             this.gbAbout.TabStop = false;
             this.gbAbout.Text = "groupBox1";
@@ -69,7 +69,7 @@
             // lblAboutDesc
             // 
             this.lblAboutDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAboutDesc.Location = new System.Drawing.Point(5, 29);
+            this.lblAboutDesc.Location = new System.Drawing.Point(5, 40);
             this.lblAboutDesc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAboutDesc.MaximumSize = new System.Drawing.Size(363, 141);
             this.lblAboutDesc.Name = "lblAboutDesc";
@@ -77,10 +77,20 @@
             this.lblAboutDesc.TabIndex = 21;
             this.lblAboutDesc.Text = "Proyecto de Fin de Grado realizado por Francisco Crespo Diez.";
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(171, 122);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(65, 76);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 25;
+            this.pictureBox2.TabStop = false;
+            // 
             // lblAboutDegree
             // 
             this.lblAboutDegree.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAboutDegree.Location = new System.Drawing.Point(5, 59);
+            this.lblAboutDegree.Location = new System.Drawing.Point(5, 70);
             this.lblAboutDegree.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAboutDegree.MaximumSize = new System.Drawing.Size(363, 141);
             this.lblAboutDegree.Name = "lblAboutDegree";
@@ -88,36 +98,26 @@
             this.lblAboutDegree.TabIndex = 22;
             this.lblAboutDegree.Text = "Grado en Ingeniería Informática";
             // 
-            // lblAboutUni
-            // 
-            this.lblAboutUni.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAboutUni.Location = new System.Drawing.Point(5, 90);
-            this.lblAboutUni.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblAboutUni.MaximumSize = new System.Drawing.Size(363, 141);
-            this.lblAboutUni.Name = "lblAboutUni";
-            this.lblAboutUni.Size = new System.Drawing.Size(231, 18);
-            this.lblAboutUni.TabIndex = 23;
-            this.lblAboutUni.Text = "Universidad de Burgos";
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(87, 111);
+            this.pictureBox1.Location = new System.Drawing.Point(87, 122);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(59, 76);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 24;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox2
+            // lblAboutUni
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(171, 111);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(65, 76);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 25;
-            this.pictureBox2.TabStop = false;
+            this.lblAboutUni.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAboutUni.Location = new System.Drawing.Point(5, 101);
+            this.lblAboutUni.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAboutUni.MaximumSize = new System.Drawing.Size(363, 141);
+            this.lblAboutUni.Name = "lblAboutUni";
+            this.lblAboutUni.Size = new System.Drawing.Size(231, 18);
+            this.lblAboutUni.TabIndex = 23;
+            this.lblAboutUni.Text = "Universidad de Burgos";
             // 
             // AboutForm
             // 
@@ -131,8 +131,8 @@
             this.Text = "About";
             this.Load += new System.EventHandler(this.About_Load);
             this.gbAbout.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
