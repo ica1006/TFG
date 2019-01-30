2f3cce69553e09aba9643e9bc645a1c5f06cda83
FranBurgos <fpizarro1990@gmail.com>
2019-01-30 18:50:45 +0100

    #70

--- PlantaPiloto/PlantaPiloto/Classes/GlobalParameters.cs
+++ PlantaPiloto/PlantaPiloto/Classes/GlobalParameters.cs
@@ -5,10 +5,26 @@ namespace PlantaPiloto.Classes
 {
     public static class GlobalParameters
     {
-        public static string FilesPath = Path.Combine(Application.StartupPath, "..\\..\\ApplicationData");
-        public static string ConfigsPath = Path.Combine(Application.StartupPath, "Configuraciones");
-        public static string DBPath = Path.Combine(Application.StartupPath, "DB");
-        public static string DBName = "TFG_DB";
-        public static float DefaultTs = 0.2F;
+        static readonly string _filesPath = Path.Combine(Application.StartupPath, "..\\..\\ApplicationData");
+        public static string FilesPath
+        {
+            get { return _filesPath; }
+        }
+        static readonly string _configsPath = Path.Combine(Application.StartupPath, "Configuraciones");
+        public static string ConfigsPath
+        {
+            get { return _configsPath; }
+        }
+        static readonly string _dBPath = Path.Combine(Application.StartupPath, "DB");
+        public static string DBPath
+        {
+            get { return _dBPath; }
+        }
+        static readonly string _dBName = "TFG_DB";
+        public static string DBName
+        {
+            get { return _dBName; }
+        }
+        public static float DefaultTs { get; } = 0.2F;
     }
 }
